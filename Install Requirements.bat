66e71092c3d629d90d8b2073a05784db167a91d5
Virusf <48204427+Virusf@users.noreply.github.com>
2025-09-17 21:53:56 +0200

    Update Install Requirements.bat

--- Install Requirements.bat
+++ Install Requirements.bat
@@ -6,7 +6,7 @@ echo.
 
 rem "C:\Python312\python" -m pip install -r requirements.txt
 python310\App\Python\python -m pip install --upgrade pip --no-warn-script-location
-python310\App\Python\python -m pip install --upgrade transformers tokenizers safetensors langdetect langid huggingface_hub --no-warn-script-location
+python310\App\Python\python -m pip install --upgrade transformers tokenizers safetensors langdetect langid huggingface_hub hf_xet --no-warn-script-location
 python310\App\Python\python -m pip install --upgrade --force-reinstall sentencepiece==0.2.0 --no-warn-script-location
 python310\App\Python\python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 --no-warn-script-location
 
