ff38f60639d5c8aa43cdfca995dbce09805b6800
Virusf <virus_f@msn.com>
2025-09-18 14:23:53 +0200

    maj install

--- Install Requirements.bat
+++ Install Requirements.bat
@@ -7,8 +7,9 @@ echo.
 rem "C:\Python312\python" -m pip install -r requirements.txt
 python310\App\Python\python -m pip install --upgrade pip --no-warn-script-location
 python310\App\Python\python -m pip install --upgrade transformers tokenizers safetensors langdetect langid huggingface_hub hf_xet peft --no-warn-script-location
+python310\App\Python\python -m pip install --upgrade torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124 --no-warn-script-location
 python310\App\Python\python -m pip install --upgrade --force-reinstall sentencepiece==0.2.0 --no-warn-script-location
-python310\App\Python\python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 --no-warn-script-location
+
 
 
 
